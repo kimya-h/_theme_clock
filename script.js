44af294b912d0785deadc0925afe03e3cf164463
kimya-h <kimya.mailbox@gmail.com>
2024-01-19 22:54:46 +0330

    dark mode active

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ const toggle = document.querySelector('.toggle')
 const days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-toggle.querySelector('click', (e) => {
+toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
-    
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
 })
